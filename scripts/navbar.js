9fc7ceb5ab7b3ae84b0fa4e87f3e93f2abc263c1
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2021-03-12 21:29:45 +0100

    reveal content while scrolling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,7 +5,7 @@ let navbar = document.querySelector("nav");
 
 
 window.addEventListener("scroll" ,()=> {
-    if (window.scrollY>50   )  {
+    if (window.scrollY>0   )  {
         header.classList.add("sticky") ;
         navbar.style.display="flex" ;
         

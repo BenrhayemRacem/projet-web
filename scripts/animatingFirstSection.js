f180975c78e831eceb54f0c73e26f30fbf6d522b
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2021-03-15 23:44:49 +0100

    ajouter section au dessus et qq fixation

---
+++ scripts/animatingFirstSection.js
@@ -0,0 +1,30 @@
+var spanArray = document.querySelectorAll("section div.container-fluid div.row h1 span");
+console.log(spanArray) ;
+
+
+
+
+
+var i =1;
+spanArray.forEach(element=>{
+    
+    element.style.animationDelay=  (i%(spanArray.length+1))* 0.05 + "s";
+    i++ ;
+})
+
+document.addEventListener("scroll" , ()=>{
+
+    if (window.scrollY>50) {
+document.querySelector("section").style.display="none";
+document.querySelector("nav").classList.add("fixed-top");
+    }
+    else{
+        document.querySelector("section").style.display="flex" ;
+document.querySelector("nav").classList.remove("fixed-top");
+    }
+
+})
+
+
+
+ 

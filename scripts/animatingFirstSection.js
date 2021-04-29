9a2ec871833fc6b66920fb59ce94241ad6b472eb
HadilHelali <helalihadil4@gmail.com>
2021-04-29 15:55:00 +0100

    Enhancing the first part of the home page

--- scripts/animatingFirstSection.js
+++ scripts/animatingFirstSection.js
@@ -24,13 +24,43 @@ document.querySelector("nav").classList.remove("fixed-top");
 
 document.querySelector(".searchbtn").addEventListener("click",(e)=> {
     document.querySelector(".searchbtn").classList.toggle("bg-Blue");
-    document.querySelector(".fa").classList.toggle("color-white");
+    document.querySelector(".fa-search").classList.toggle("color-white");
     document.querySelector(".input").classList.toggle("active-width");
     
 })
 
 
 
+// CountUp  
+function animateValues() {
+    var duration = 500 ;
+    var start = 0 ;
+    var ListObj = document.querySelectorAll(".Number");
+    for (i=0;i<ListObj.length ; i++)
+    {   if (ListObj[i].innerHTML>1000)
+        {duration = 50 ; start = 2000 ;} 
+        animateValue(ListObj[i],start,ListObj[i].innerHTML,duration);
+    }
+}
 
+animateValues();
+
+
+function animateValue(obj, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+   
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+// End CountUp  
 
  

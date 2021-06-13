75a8aab77d8d84aa990a621275b8c7f67fafdb40
HadilHelali <helalihadil4@gmail.com>
2021-06-13 12:26:14 +0100

    Fixing the script

--- public/Scripts/UserScripts/UserScripts.js
+++ public/Scripts/UserScripts/UserScripts.js
@@ -1,10 +1,10 @@
 document.querySelector(".UserIcon").addEventListener('mouseenter' , (e)=> {
     document.querySelector(".UserIcon").style.backgroundImage = "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)" ;
-    document.querySelector(".fa-user").style.color = "white" ;
 })
 
 
 document.querySelector(".UserIcon").addEventListener('mouseleave' , (e)=> {
-    document.querySelector(".UserIcon").style.background = "white" ;
-    document.querySelector(".fa-user").style.color = "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)" ;
+    document.querySelector(".UserIcon").style.background = "#5EC2C2" ;
+    document.querySelector(".UserIcon").style.backgroundImage = "#5EC2C2" ;
+
 })
